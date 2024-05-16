3ae3cf8fc7a74fa0bb8a03dbbb3a0affa385f29c
Snaky1a <snaky1a@outlook.com>
2024-05-16 20:44:57 +0300

    ww

--- utils.sh
+++ utils.sh
@@ -271,6 +271,7 @@ get_uptodown_vers() { $HTMLQ --text ".version" <<<"$__UPTODOWN_RESP__"; }
 dl_uptodown() {
 	local uptodown_dlurl=$1 version=$2 output=$3
 	local url
+	echo $output
 	url=$(grep -F "${version}</span>" -B 2 <<<"$__UPTODOWN_RESP__" | head -1 | sed -n 's;.*data-url=".*download\/\(.*\)".*;\1;p') || return 1
 	url="https://dw.uptodown.com/dwn/$(req "${uptodown_dlurl}/post-download/${url}" - | sed -n 's;.*class="post-download" data-url="\(.*\)".*;\1;p')" || return 1
 	req "$url" "$output"
